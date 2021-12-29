af7348c7c77df17d9fc0239fdb1a1a6a2dff8d22
Shubham Yadav <shubh08am@gmail.com>
2021-12-29 20:04:47 +0530

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+// while ( 1<=t <= 100)           
+            if ( t%2== 1 )
+            cout<<"NO\n";
+           
+           if ( t == 2 )
+           cout<<"NO\n";
+         
+           else if ( t%2 == 0)
+           cout<<"YES\n";
+       
+    return 0 ;
+}
