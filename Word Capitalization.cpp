9fb1582f0ac39499b84d7024c1d65daaeae5acfa
Shubham Yadav <shubh08am@gmail.com>
2022-01-15 19:08:16 +0530

    Create Word Capitalization.cpp

---
+++ Word Capitalization.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+string s;
+cin >> s;
+    if ( 97<=s[0]<=122)
+
+   { 
+    //s[0] = toupper(s[0]);
+     s[0] = towupper(s[0]);
+    cout<< s;
+}
+
+return 0;
+}
