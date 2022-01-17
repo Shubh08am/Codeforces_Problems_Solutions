2d80be58a8354f49d779b134c042a3914e74ed88
Shubham Yadav <shubh08am@gmail.com>
2022-01-17 15:00:48 +0530

    Create Equidistant Letters.cpp

---
+++ Equidistant Letters.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+#include<string>
+
+using namespace std;
+
+int main()
+
+{
+
+		int t;
+
+	cin>>t;
+
+	while(t--){
+
+		
+
+		string s;
+
+		cin>>s;
+
+		sort(s.begin() , s.end());
+
+		cout<<s<<"\n";		
+
+		
+
+	}	
+
+	return 0;
+
+}
