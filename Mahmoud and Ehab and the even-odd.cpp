02bb94f6b9abdb2144187b9410e5de68f74c92e8
Shubham Yadav <shubh08am@gmail.com>
2022-01-21 21:29:40 +0530

    Create Mahmoud and Ehab and the even-odd.cpp

---
+++ Mahmoud and Ehab and the even-odd.cpp
@@ -0,0 +1,24 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+int main()
+{
+/*int t;
+cin>> t ;
+while (t--)
+{*/ 
+
+
+long long n;
+cin>>n;
+
+if ( n%2 == 0)
+cout << "Mahmoud\n";
+else
+cout <<"Ehab" << endl;
+
+return 0;
+
+}
