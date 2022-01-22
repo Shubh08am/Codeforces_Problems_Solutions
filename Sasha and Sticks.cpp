c560ad7e818196cc4616d240cef647a3bea2bdc6
Shubham Yadav <shubh08am@gmail.com>
2022-01-22 19:15:10 +0530

    Create Sasha and Sticks.cpp

---
+++ Sasha and Sticks.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+#include<vector>
+
+using namespace std; 
+
+int main()
+
+{
+
+/*int t;
+
+cin>> t ;
+
+while (t--)
+
+{*/ 
+
+long long n , k;
+
+cin>>n>>k;
+
+int cnt = n / k; 
+
+    if( cnt % 2 == 0)
+
+     cout << "NO\n";
+
+     
+
+    else 
+
+    cout<<"YES\n" ;
+
+    
+
+return 0;
+
+}
+
+ 
