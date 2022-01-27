5d0cc061d76cdb58fa233f063899e43918a92c51
Shubham Yadav <shubh08am@gmail.com>
2022-01-27 15:32:09 +0530

    Create Again Twenty Five!.cpp

---
+++ Again Twenty Five!.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std; 
+
+int main()
+{ 
+
+long long int n;
+//long long n;
+cin>>n;
+//unsigned m = pow(5,n);
+
+//	r = m%10;
+//	s = m%100;
+
+cout << 25<< endl;
+
+
+return 0; 
+
+}
