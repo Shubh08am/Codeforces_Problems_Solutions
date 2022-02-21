63320c3d34c2559a3429f1068ad979b6580da5fa
Shubham Yadav <shubh08am@gmail.com>
2022-02-21 15:54:48 +0530

    Create Consecutive Sum Riddle.cpp

---
+++ Consecutive Sum Riddle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define ll long long 
+
+int main() { 
+
+int t;
+cin >> t;
+while(t--){
+	
+ll int n;
+cin >> n;
+ll m = -n+1;
+ll y = 1;
+
+cout <<m<< " " << n<< endl; 
+
+}
+return 0;
+}
