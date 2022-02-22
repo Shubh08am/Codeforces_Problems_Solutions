2a405167a238167a5ff3deed183337d79a46134d
Shubham Yadav <shubh08am@gmail.com>
2022-02-22 19:44:45 +0530

    Create Fence.cpp

---
+++ Fence.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main() { 
+
+int t;
+cin >> t;
+while(t--){
+
+		ll v[3];
+cin >> v[0] >> v[1] >> v[2];
+
+sort(v,v+3);
+
+cout <<v[2] <<endl;
+}
+return 0;
+}
