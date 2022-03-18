5f2af20384e56915887210d5a74bf0a46d55cd1c
Shubham Yadav <shubh08am@gmail.com>
2022-03-18 18:29:01 +0530

    Create Quirky Quantifiers.cpp

---
+++ Quirky Quantifiers.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define  ld long double
+#define int long long
+
+
+#define vl vector<ll>
+
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
+#define rev(i, a, b) for (ll i = a; i >=b; --i)
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define F first
+#define S second
+#define lb lower_bound
+#define ub upper_bound
+#define setp(x) setprecision(x) << fixed
+#define in insert
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+ 
+#define Sort(x) sort(all(x))
+#define out(x) cout << x << "\n";
+#define trav(i, v) for (auto &i : v)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define take(v) trav(i, v) cin >> i;
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+ 
+
+const char nl = '\n';
+ 
+ll inf = 1e18;
+ll mod = 1000000000 + 7;
+int MAXN = 1010101;
+
+unsigned int gcd(unsigned int a ,unsigned int b)
+{
+    /* base condition*/ 
+    if(b==0) return a;
+    return gcd (b,a%b );
+}
+
+ll lcm(ll a, ll b){
+    return (a*b)/gcd(a,b);
+}
+
+ll  fact(ll  n){
+    if(n==0) return 1;
+
+    return n *fact(n-1);
+}
+
+signed main(){
+/*ll t;
+cin>>t;
+while(t--){*/
+
+
+ 
+ll n  , s     ;  
+cin>>n    ;
+if(n&1) cout<<1<<nl;
+
+else cout<<0<<nl;
+
+
+//}
+
+return 0;}
