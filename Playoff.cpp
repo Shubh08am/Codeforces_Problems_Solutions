acd491b31d335a6b50b7fed92c6f9bdc38aa76a7
Shubham Yadav <shubh08am@gmail.com>
2022-03-28 18:47:06 +0530

    Create Playoff.cpp

---
+++ Playoff.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+ 
+
+#define rep(i,a,b) for (int i=a;i<b;i++)
+
+#define ll long long 
+
+ 
+
+int main (){
+
+ 
+
+
+
+
+
+   
+
+
+
+    ll t;
+
+    cin>>t ;
+
+    while (t--){
+
+       ll n ;
+
+       cin>> n ;
+
+
+
+  
+
+       
+
+   
+
+      
+
+         
+
+     
+
+
+
+      ll m = pow(2,n)-1;
+
+      cout << m << "\n";
+
+    
+
+    }
+
+}
