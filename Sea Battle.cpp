4035b5a9d7973c318391eeb4bbd4391e7ab71dd5
Shubham Yadav <shubh08am@gmail.com>
2022-04-03 18:40:03 +0530

    Create Sea Battle.cpp

---
+++ Sea Battle.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std; 
+
+#define ll long long
+
+
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
+#define all(x) (x).begin(), (x).end() 
+
+#define Sort(x) sort(all(x))
+const char nn = '\n'; 
+
+ll inf = 1e18;
+ll mod = 1000000000 + 7;
+int MAXN = 1010101; 
+
+signed main(){
+
+ll xx;
+ll w1,w2,h1,h2;
+cin>>w1>>h1>>w2>>h2;
+
+xx = 2*(h1+h2+w1+2);
+
+
+cout<< xx;
+
+return 0;
+}
