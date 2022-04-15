efc54272a138bc1a0aae0ada717115aa50347fc4
Shubham Yadav <shubh08am@gmail.com>
2022-04-15 20:04:23 +0530

    Create Vasya and Coins.cpp

---
+++ Vasya and Coins.cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+#define  ld long double
+
+#define int long long
+
+#define vl vector<ll>
+
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
+
+#define rev(i, a, b) for (ll i = a; i >=b; --i)
+
+#define fo(i, n) for (ll i = 0; i < n; i++)
+
+#define F first
+
+#define S second
+
+#define lb lower_bound
+
+#define ub upper_bound3
+
+#define setp(x) setprecision(x) << fixed
+
+#define in insert
+
+#define pb push_back
+
+#define mp make_pair
+
+#define all(x) (x).begin(), (x).end()
+
+ 
+
+#define Sort(x) sort(all(x))
+
+#define out(x) cout << x << "\n";
+
+#define trav(i, v) for (auto &i : v)
+
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+
+#define take(v) trav(i, v) cin >> i;
+
+#define no cout<<"No"<<endl
+
+#define yes cout<<"Yes"<<endl
+
+#define str string
+
+const char nl = '\n';
+
+ 
+
+ll inf = 1e18;
+
+ll mod = 1000000000 + 7;
+
+int MAXN = 1010101;
+
+unsigned int gcd(unsigned int a ,unsigned int b)
+
+{
+
+    /* base condition*/ 
+
+    if(b==0) return a;
+
+    return gcd (b,a%b );
+
+}
+
+ll lcm(ll a, ll b){
+
+    return (a*b)/gcd(a,b);
+
+}
+
+ll  fact(ll  n){
+
+    if(n==0) return 1;
+
+    return n *fact(n-1);
+
+}
+
+signed main(){
+
+ll t;
+
+cin>>t;
+
+while(t--){
+
+ll n ,m    ;
+
+cin>>  n  >>  m            ;
+
+   
+
+   if(n==0 and m>0) cout<<1<<nl;
+
+   
+
+   else if(n==0 and m==0) cout<<1<<nl;
+
+   
+
+   else{ 
+
+       ll o = n+2*m+1;
+
+       cout<< o<<nl;
+
+   }
+
+   
+
+      //   ll a[n]; 
+
+     //  rep(i,0,n)  cin>>a[i];
+
+//cout<<    <<nl;
+
+}
+
+return 0;}
